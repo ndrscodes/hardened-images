3800d02a51e9b76abbbc6e903a572a11fa1d773b
Andreas Schmidt <ndrscodes@gmail.com>
2026-08-28 13:23:44 +0200

    feat: set up pipeline

---
+++ .github/cicd/detect-changed-images.sh
@@ -0,0 +1,17 @@
+CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r ${{ github.sha }})
+
+DIRS=()
+for file in $CHANGED_FILES; do
+    DIR=$(echo "$file" | cut -d'/' -f1)
+    if [ -d "$DIR" ] && [ -f "$DIR/Dockerfile" ]; then
+        DIRS+=("$DIR")
+    fi
+done
+
+if [ ${#DIRS[@]} -eq 0 ]; then
+    JSON_ARRAY="[]"
+else
+    JSON_ARRAY=$(printf '"%s"\n' "${DIRS[@]}" | sort -u | uniq | jq -s -c .)
+fi
+
+echo "$JSON_ARRAY"
