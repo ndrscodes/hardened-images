8c5b112d06d9757796ee83636f3dcf1c62bd11c8
Andreas Schmidt <ndrscodes@gmail.com>
2026-09-01 12:46:12 +0200

    feat: log changed files

--- .github/cicd/detect-changed-images.sh
+++ .github/cicd/detect-changed-images.sh
@@ -1,4 +1,5 @@
 if [ -n "$TARGET" ]; then
+    echo "target is set ($TARGET) - running non-git based build" >&2
     if [ "$TARGET" = "all" ]; then
         CHANGED_FILES=$(find . -name Dockerfile)
     else 
@@ -6,11 +7,13 @@ if [ -n "$TARGET" ]; then
     fi
 else
     CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r $GITHUB_SHA | sed 's|^\./||g')
+    echo "changed: $(git diff-tree --no-commit-id --name-only -r $GITHUB_SHA | sed 's|^\./||g')" >&2
 fi
 
 DIRS=()
 for file in $CHANGED_FILES; do
     DIR=$(echo "$file" | cut -d'/' -f1)
+    echo "$file" | cut -d'/' -f1 >&2
     if [ -d "$DIR" ] && [ -f "$DIR/Dockerfile" ]; then
         DIRS+=("$DIR")
     fi
