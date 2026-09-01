f5b1fd27b48bb60534255b650b1c67354f2bc7c4
Andreas Schmidt <ndrscodes@gmail.com>
2026-09-01 11:46:56 +0200

    feat: improve pipeline metadata and build

--- .github/cicd/calculate-tags.sh
+++ .github/cicd/calculate-tags.sh
@@ -1,11 +1,28 @@
 IMAGE_DIR="${MATRIX_IMAGE}"
 DOCKERFILE="${IMAGE_DIR}/Dockerfile"
+SETTINGS_ENV="${IMAGE_DIR}/settings.env"
+if [ -f $SETTINGS_ENV ]; then
+    source $SETTINGS_ENV
+fi
 
-UPSTREAM_VER=$(grep -w -E "(FROM )|(COPY --from=)[a-zA-Z0-9/.]*$IMAGE_DIR:\S*" $IMAGE_DIR/Dockerfile | head -n 1 | sed -E 's/.*:([a-zA-Z0-9\._-]+)(@.+)?/\1/g')
+if [ -z "$IMAGE_NAME" ]; then
+    IMAGE_NAME=$MATRIX_IMAGE
+fi
+
+if [ -n "$IMAGE_VERSION" ]; then
+    UPSTREAM_VER=$IMAGE_VERSION
+else
+    UPSTREAM_VER=$(grep -w -E "(FROM )|(COPY --from=)[a-zA-Z0-9/.]*$IMAGE_DIR:\S*" $IMAGE_DIR/Dockerfile | head -n 1 | sed -E 's/.*:([a-zA-Z0-9\._-]+)(@.+)?/\1/g')
+fi
+
 [[ "$UPSTREAM_VER" =~ ^v ]] || UPSTREAM_VER="v${UPSTREAM_VER}"
 
+if [ -n "$IMAGE_VARIANT" ]; then
+    UPSTREAM_VER="${UPSTREAM_VER}-$IMAGE_VARIANT"
+fi
+
 # Look up highest revision for this specific image prefix
-GIT_PREFIX="${IMAGE_DIR}/${UPSTREAM_VER}"
+GIT_PREFIX="${IMAGE_NAME}/${UPSTREAM_VER}"
 LATEST_TAG=$(git tag -l "${GIT_PREFIX}-r*" | sort -V | tail -n 1)
 
 if [ -z "$LATEST_TAG" ]; then
@@ -18,3 +35,6 @@ fi
 echo "GIT_TAG=${GIT_PREFIX}-r${NEW_REV}" > $GITHUB_ENV
 echo "IMAGE_TAG=${UPSTREAM_VER}-r${NEW_REV}" >> $GITHUB_ENV
 echo "VERSION_ALIAS=${UPSTREAM_VER}" >> $GITHUB_ENV
+
+ARGS=$(cat $SETTINGS_ENV | grep -v "^\w*#" | tr -s '\n' | paste -sd ',' -)
+echo "build_args=$ARGS" >> $GITHUB_OUTPUT
